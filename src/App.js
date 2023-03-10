2535f28b3d8236920ff9fd9ce4f9925059e3d54c
sruthi <sruthikallingal@gmail.com>
2023-03-10 16:17:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import About from './Container/About';
+import Profile from './Container/Profile'
+import {useState} from 'react'
+import {BrowserRouter as Router,Link,Routes,Route} from 'react-router-dom'
+import {AppContext} from './AppContext'
 
 function App() {
+  const [state,setState] = useState(20)
+  // let component
+  // if (state === 'about'){
+  //   component = <About />
+  // }
+  // if (state === 'profile'){
+  //   component = <Profile />
+  // }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <button onClick={()=>setState('about')}>About</button>
+      <button onClick={()=>setState('profile')}>Profile</button> */}
+      <AppContext.Provider value={{data:state}}>
+      <Router>
+            <Link to='/about'>About</Link>
+            <Link to='/profile'>Profile</Link>
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/profile" element={<Profile data={state} />} />
+          </Routes>
+         
+      </Router>
+      </AppContext.Provider>
+      
     </div>
   );
 }
---
+++ src/Container/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import One from '../component/One'
+
+function profile(props) {
+  return (
+    <div style={{backgroundColor:'blue'}}>
+      <h1>Profile</h1>
+     <One ></One>
+    </div>
+  )
+}
+
+export default profile
